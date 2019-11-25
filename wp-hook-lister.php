d214543fc2fc227810bf6786ca84b5cf4b00ec94
Julien MA JACOB <juulien.jacob@gmail.com>
2019-11-25 02:09:14 +0100

    Clear test code

--- wp-hook-lister.php
+++ wp-hook-lister.php
@@ -206,26 +206,6 @@ foreach ( $php_files as $key => $php_file ) {
 	}
 }
 
-function slugifier($txt){
-
-	/* Get rid of accented characters */
-	$search = explode(",","ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,e,i,ø,u");
-	$replace = explode(",","c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,e,i,o,u");
-	$txt = str_replace($search, $replace, $txt);
- 
-	/* Lowercase all the characters */
-	$txt = strtolower($txt);
- 
-	/* Avoid whitespace at the beginning and the ending */
-	$txt = trim($txt);
- 
-	/* Replace all the characters that are not in a-z or 0-9 by a hyphen */
-	$txt = preg_replace("/[^a-z0-9]/", "-", $txt);
-	/* Remove hyphen anywhere it's more than one */
-	$txt = preg_replace("/[\-]+/", '-', $txt);
-	return $txt;   
- }
-
 
 /**
  * Generate HTML and MarkDown content
